c2cc502cb07814b78af9068a2ce4c1b30ba0bf29
AM <144672669+AMorid@users.noreply.github.com>
2024-11-29 02:05:38 +0100

    Les van 28 nov toepassen! lineaire_regressie_gradient_descent.

--- lineaire_regressie_gradient_descent.py
+++ lineaire_regressie_gradient_descent.py
@@ -1,6 +1,10 @@
 # Team FaaaM,
-# Opdracht: Steam
-# Bronen: W3school, more ...
+# Opdracht: Steam_AI_deel
+# Bronen: Uit de les, W3school, and more ...
 import os
 import matplotlib.pyplot as plt
 
+'''
+Er komt alles over AI gedeelte, applied based on AI les van donderdag 28 nov.
+
+'''
